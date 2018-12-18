6ccd1b139a7aa11f336c829358dc778166d1e374
Vanessa Prema <vanessa.prema@flatironschool.com>
2018-12-18 13:11:33 -0500

    Banking OO

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  @@senders = []
+  @@receivers = []
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if @status == "complete"
+      "Transaction complete."
+    elsif @sender.valid? && @sender.balance > @amount
+      @receiver.deposit(@amount)
+      @sender.deposit(@amount * -1)
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.deposit(@amount * -1)
+      @sender.deposit(@amount)
+      @status = "reversed"
+    end
+  end
+
 end
